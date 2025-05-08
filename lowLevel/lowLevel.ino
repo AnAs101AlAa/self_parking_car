18a03cce14b8a045b531c98ed681eb27c345250e
Omar Elshereef <omarelshereef@gmail.com>
2025-05-08 12:32:56 +0300

    refactor: clean up code formatting and remove redundant includes

--- lowLevel/lowLevel.ino
+++ lowLevel/lowLevel.ino
@@ -6,12 +6,9 @@
 #include "sensor_utils.h"
 #include "BluetoothSerial.h"
 
-#include "BluetoothSerial.h"
-
-
 int motorPins[4] = {26, 27, 25, 33};
-const int triggerPins[5] = {22,19,18,17,4}; //LF LB RF RB BB
-const int echoPins[5] = {23,21,5,16,2}; //LF LB RF RB BB
+const int triggerPins[5] = {22, 19, 18, 17, 4}; // LF LB RF RB BB
+const int echoPins[5] = {23, 21, 5, 16, 2};     // LF LB RF RB BB
 long distances[5];
 int counterLeft;
 int counterRight;
@@ -20,11 +17,13 @@ int initializePark;
 char cmd;
 BluetoothSerial serialBt;
 
-void setup() {
+void setup()
+{
     Serial.begin(115200);
     initHardwareTimer();
 
-    for(int i=0;i< 5;i++) {
+    for (int i = 0; i < 5; i++)
+    {
         setOutput(triggerPins[i]);
         distances[i] = 0;
     }
@@ -36,97 +35,102 @@ void setup() {
     initializePark = -1;
 
     motorsInit(motorPins);
-    //carForward(70);
+    // carForward(70);
 }
 
-void loop() {
+void loop()
+{
 
-    if(serialBt.available()){
+    if (serialBt.available())
+    {
         cmd = serialBt.read();
     }
-    if(cmd=='1'){
-        initializePark= 0;
+    if (cmd == '1')
+    {
+        initializePark = 0;
         carForward(70);
-    }else if (cmd == '0'){
+    }
+    else if (cmd == '0')
+    {
         initializePark = -1;
         carStop();
     }
 
-
-
-
-    for(int i=0;i< 5; i++) {
+    for (int i = 0; i < 5; i++)
+    {
         distances[i] = measureDistance(triggerPins[i], echoPins[i]);
         Serial.print("sensor: ");
-        Serial.print(i+1);
+        Serial.print(i + 1);
         Serial.print(" value: ");
         Serial.println(distances[i]);
     }
 
-    if(serialBt.available()){
-      cmd = serialBt.read();
-    }
-
-    if(cmd=='1'){
-      initializePark = 0;
-      carForward(100);
-      cmd = 'N';
-    }else if (cmd=='0')
+    if (initializePark == 0)
     {
-      initializePark = -1;
-      cmd = 'N';
-    }
-
-
-    if(initializePark == 0) {
-        if(distances[0] > 23 && distances[1] > 23) {
-            if(counterLeft >= 6) {
+        if (distances[0] > 23 && distances[1] > 23)
+        {
+            if (counterLeft >= 6)
+            {
                 initializePark = 1;
                 Serial.println("parking left");
                 delay(10);
-            } else {
+            }
+            else
+            {
                 counterLeft++;
                 delay(2);
             }
-        } else if (!(distances[0] > 23 && distances[1] > 23)) {
+        }
+        else if (!(distances[0] > 23 && distances[1] > 23))
+        {
             counterLeft = 0;
         }
 
-        if(distances[2] > 23 && distances[3] > 23) {
-            if(counterRight >= 6) {
+        if (distances[2] > 23 && distances[3] > 23)
+        {
+            if (counterRight >= 6)
+            {
                 initializePark = 2;
                 Serial.println("parking right");
                 delay(10);
-            } else {
+            }
+            else
+            {
                 counterRight++;
             }
-        } else if (!(distances[2] > 23 && distances[3] > 23)) {
+        }
+        else if (!(distances[2] > 23 && distances[3] > 23))
+        {
             counterRight = 0;
         }
     }
 
-    else if(initializePark == 1) {
+    else if (initializePark == 1)
+    {
         carStop();
         delay(100);
         carSpinLeft(740);
         carBackward(60);
         delay(300);
-        while(true) {
-            if(measureDistance(triggerPins[4], echoPins[4]) < 2)
+        while (true)
+        {
+            if (measureDistance(triggerPins[4], echoPins[4]) < 2)
                 break;
         }
         carStop();
         initializePark = 3;
     }
-    
-    else if(initializePark == 2) {
+
+    else if (initializePark == 2)
+    {
         carStop();
         delay(100);
         carSpinRight(740);
         carBackward(60);
         delay(300);
-        while(true) {
-            if(measureDistance(triggerPins[4], echoPins[4]) < 2)
+        while (true)
+        {
+            if (measureDistance(triggerPins[4], echoPins[4]) < 2)
                 break;
         }
         carStop();
